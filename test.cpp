513dedcbf947e7be811dc5b2f3abbc6097a0432d
Amit Gohri <amit13542.ag@gmail.com>
2020-05-15 18:54:39 +0530

    changes in test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,43 @@
+//current visual studio code
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
-int main(){
-    vector<int> A{1,2,3,4,5};
-    cout << A.size();
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int result(0);
+        int N;
+        cin >> N;
+        vector<int> inp(N);
+        stack<int> s;
+        int p;
+        for (int i = 0; i < N; i++)
+        {
+            cin >> inp[i];
+            if (s.empty() || inp[i] < inp[s.top()])
+            {
+                s.push(i);
+                p = i;
+            }
+            else
+            {
+                s.push(p);
+            }
+        }
+        while (s.size() != 0) {
+            {
+                result += s.size() * inp[s.top()];
+                for(int i = 0 ; i<s.top()+1 ; i++ )
+                {
+                    s.pop();
+                }
+            }
+
+
+        }
+
+    }
 }
